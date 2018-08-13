7965cc149778204872b9928bde71a4ea7d8869e9
harmanjitsodhi <harmanjitsodhi@yahoo.com>
2018-08-13 15:42:39 -0700

    made model schemas

--- server/models/models.js
+++ server/models/models.js
@@ -13,5 +13,53 @@ var userSchema = new mongoose.Schema({
   }
 });
 
+var quizSchema = new mongoose.Schema({
+  quizTitle: {
+    type: String,
+    required: true
+  },
+  teacher: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User'
+  },
+  isComplete: {
+    type: Boolean,
+    required: true
+
+  }
+
+})
+
+var scoreSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User'
+  },
+  quiz: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Quiz'
+  },
+  score: {
+    type: String,
+    required: true
+  }
+})
+
+var questionsSchema = new mongoose.Schema({
+  quiz: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Quiz'
+  },
+  questions: [{
+    type: String,
+    required: true
+
+
+  }]
+})
+
 const User = mongoose.model('User', userSchema);
-module.exports = {User};
+const Quiz = mongoose.model('Quiz', quizSchema);
+const Questions = mongoose.model('Questions', questionSchema);
+const Score = mongoose.model('Score', scoreSchema);
+module.exports = {User, Quiz, Questions, Score};
